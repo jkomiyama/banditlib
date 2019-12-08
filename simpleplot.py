1786ab6e0befb343058a1255ba8ba44d543a2416
Junpei <junpeikomiyama@gmail.com>
2019-12-08 18:53:38 -0500

    quick fix for Python3 compatibility

--- simpleplot.py
+++ simpleplot.py
@@ -1,10 +1,15 @@
 #!/usr/env/python
 #coding:utf-8
 
+USE_DISPLAY = True
+
+import matplotlib as mpl
+if not USE_DISPLAY:
+  mpl.use('Agg')
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
-import matplotlib as mpl
 import sys, os, copy, math, re
 
 def thin(anarray):
@@ -18,7 +23,7 @@ def thin(anarray):
   return retarray
 
 def readFile(afile):
-  lines = [line.strip() for line in file(afile, "r").readlines()]
+  lines = [line.strip() for line in open(afile, "r").readlines()]
   algs = []
   data = []
   for line in lines:
@@ -29,23 +34,25 @@ def readFile(afile):
       sps = line.split(" ")
       data.append(sps)
   data = map(list, zip(*data)) #transpose 2d array
-  data = map(thin, data) #reduce data points
+  data = list(map(thin, data)) #reduce data points
   return (algs, data)
 
-def plotRegret((algs, data)):
+def plotRegret(args):
+  (algs, data) = args
   plt.xlabel("t: round")
   plt.ylabel("R(t): regret")
   plt.xscale('log')
   for l in range(2, len(data), 2):
-    i=l/2-1
+    i=l//2-1
     plt.plot(data[0], data[l], "-", label=algs[i], linewidth=2)
   plt.legend(loc="upper left")
   plt.savefig('regret.pdf')
-  plt.show()
+  if USE_DISPLAY:
+    plt.show()
 
 if __name__ == '__main__':
   if len(sys.argv)<2:
-    print "usage: this logfile"
+    print("usage: this logfile")
     sys.exit()
   afile = sys.argv[1]
   result = readFile(afile)
